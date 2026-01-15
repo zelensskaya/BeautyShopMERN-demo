a9679d94308f615fc0582dc56c4fdbb0bbf47e28
Marina Zelenska <zele.ma1802@gmail.com>
2026-01-15 18:13:11 +0100

    Home page design

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Lash Sensational Mascara',
+    image: '/images/maybelline-lash.jpg',
+    description:
+      'Maybelline Lash Sensational Mascara, Blackest Black - 212',
+    brand: 'Maybelline',
+    category: 'Makeup',
+    price: 8.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'Super Stay Vinyl Ink Lipstick',
+    image: '/images/maybelline-superstay-vinyl-ink.jpg',
+    description:
+      'Super Stay Vinyl Ink Lipstick - 100 Charmed | Maybelline NY',
+    brand: 'Maybelline',
+    category: 'Makeup',
+    price: 5.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Garnier Fructis Fresh Strengthening Shampoo',
+    image: '/images/garnier-fructis-fresh-szampon.jpg',
+    description:
+      'Garnier Fructis Fresh Strengthening Shampoo for Normal Hair 400ML',
+    brand: 'Garnier',
+    category: 'Hair',
+    price: 19.19,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'L\'oreal Elseve Hyaluron Pure Shampoo',
+    image: '/images/loreal-elseve-szampon.jpg',
+    description:
+      'L\'oreal Elseve Hyaluron Pure 72H Cleansing Hair Shampoo 400ML',
+    brand: 'L\'oreal',
+    category: 'Hair',
+    price: 39.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'ISANA MEN EXTRA POWER Shampoo',
+    image: '/images/isana-men-szampon.jpg',
+    description:
+      'ISANA MEN EXTRA POWER Shampoo for Normal Hair',
+    brand: 'ISANA',
+    category: 'Men',
+    price: 21.50,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'La Rive I Am Ideal Eau De Parfum',
+    image: '/images/la-rive-i-am-ideal-eau-de-parfum.jpg',
+    description:
+      'La Rive I Am Ideal Eau De Parfum (90ml)',
+    brand: 'La Rive',
+    category: 'Perfume',
+    price: 49.60,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+export default products
