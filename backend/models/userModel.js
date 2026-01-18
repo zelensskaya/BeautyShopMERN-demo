b94f298807c3dc820c6b94a65c8bbed0b166162f
Marina Zelenska <zele.ma1802@gmail.com>
2026-01-18 21:20:46 +0100

    Backend Authentification

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,28 +1,47 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
-const userShema = new mongoose.Schema({
+const userSchema = mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     isAdmin: {
-        type: Boolean,
-        required: true,
-        default: false,
-    },    
-},{
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
     timestamps: true,
+  }
+);
+
+// Match user entered password to hashed password in database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Encrypt password using bcrypt
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) {
+    return;
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
-const User = mongoose.model('User', userShema);
+const User = mongoose.model('User', userSchema);
 
 export default User;
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,31 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
+
 const port = process.env.PORT || 5000;
+
+connectDB(); // Connect to MongoDB
 
 const app = express();
 
-connectDB(); // Connect to MongoDB
+// Body parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// Cookie parser Middleware
+app.use(cookieParser());
+
 
 app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
